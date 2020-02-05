d3da7d714d463d9ecea7fff1fec7f346acaba42e
kunalsaraf <saraffkunal@gmail.com>
2020-02-05 22:36:26 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,146 @@
+grid = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+a = 'O'
+b = 'X'
+
+
+def checkWinner(grid):
+    # First row player 1
+    if grid[0][0] == a and grid[0][1] == a and grid[0][2] == a:
+        return -1
+    # First row player 2
+    if grid[0][0] == b and grid[0][1] == b and grid[0][2] == b:
+        return 1
+    # Second row player 1
+    if grid[1][0] == a and grid[1][1] == a and grid[1][2] == a:
+        return -1
+    # Second row player 2
+    if grid[1][0] == b and grid[1][1] == b and grid[1][2] == b:
+        return 1
+    # Third row player 1
+    if grid[2][0] == a and grid[2][1] == a and grid[2][2] == a:
+        return -1
+    # Third row player 2
+    if grid[2][0] == b and grid[2][1] == b and grid[2][2] == b:
+        return 1
+    # First column player 1
+    if grid[0][0] == a and grid[1][0] == a and grid[2][0] == a:
+        return -1
+    # First column player 2
+    if grid[0][0] == b and grid[1][0] == b and grid[2][0] == b:
+        return 1
+    # Second column player 1
+    if grid[0][1] == a and grid[1][1] == a and grid[2][1] == a:
+        return -1
+    # Second column player 2
+    if grid[0][1] == b and grid[1][1] == b and grid[2][1] == b:
+        return 1
+    # Third column player 1
+    if grid[0][2] == a and grid[1][2] == a and grid[2][2] == a:
+        return -1
+    # Third column player 2
+    if grid[0][2] == b and grid[1][2] == b and grid[2][2] == b:
+        return 1
+    # Major diagonal player 1
+    if grid[0][0] == a and grid[1][1] == a and grid[2][2] == a:
+        return -1
+    # Major diagonal player 2
+    if grid[0][0] == b and grid[1][1] == b and grid[2][2] == b:
+        return 1
+    # Minor diagonal player 1
+    if grid[0][2] == a and grid[1][1] == a and grid[2][0] == a:
+        return -1
+    # Minor diagonal player 2
+    if grid[0][2] == b and grid[1][1] == b and grid[2][0] == b:
+        return 1
+    for i in grid:
+        for j in i:
+            if j == '-':
+                return 0
+    return 99
+
+
+def printGrid(grid):
+    for i in grid:
+        for j in i:
+            print(j, end=' ')
+        print()
+
+
+def markMyEntry(position, player):
+    if position not in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']:
+        return False
+    if player == 1:
+        symbol = a
+    else:
+        symbol = b
+    if position == 'a':
+        if grid[0][0] != '-':
+            return False
+        grid[0][0] = symbol
+        return True
+    if position == 'b':
+        if grid[0][1] != '-':
+            return False
+        grid[0][1] = symbol
+        return True
+    if position == 'c':
+        if grid[0][2] != '-':
+            return False
+        grid[0][2] = symbol
+        return True
+    if position == 'd':
+        if grid[1][0] != '-':
+            return False
+        grid[1][0] = symbol
+        return True
+    if position == 'e':
+        if grid[1][1] != '-':
+            return False
+        grid[1][1] = symbol
+        return True
+    if position == 'f':
+        if grid[1][2] != '-':
+            return False
+        grid[1][2] = symbol
+        return True
+    if position == 'g':
+        if grid[2][0] != '-':
+            return False
+        grid[2][0] = symbol
+        return True
+    if position == 'h':
+        if grid[2][1] != '-':
+            return False
+        grid[2][1] = symbol
+        return True
+    if position == 'i':
+        if grid[2][2] != '-':
+            return False
+        grid[2][2] = symbol
+        return True
+
+
+printGrid(grid)
+chance = 1
+
+while checkWinner(grid) == 0:
+    print("Enter a location: ",end=' ')
+    position = input()
+    if chance == 1:
+        if markMyEntry(position, 1):
+            chance = 2
+        else:
+            print("Invalid Move")
+    else:
+        if markMyEntry(position, 2):
+            chance = 1
+        else:
+            print("Invalid Move")
+    printGrid(grid)
+
+if (checkWinner(grid) == 1):
+    print("Winner is B")
+elif (checkWinner(grid) == -1):
+    print("Winner is A")
+else:
+    print("Game Drawn")
